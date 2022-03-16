beb1474c85b252e4821cc6951c8e70f0b1e96f78
Randil Tennakoon <randil2016al@gmail.com>
2022-03-17 04:00:56 +0530

    updated App.ja and added Post.js & Post.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+// import Post from './Post';
 
-function App() {
+// It's better to write like this.
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>This is a new part</h2>
+      {/* <Post /> */}
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Post.css";
+
+class Post extends Component{
+    render(){
+        return(
+            <div className="Post">
+                <h2>This is inside the Post</h2>
+            </div>
+        );
+    }
+}
+
+export default Post;
